e3cbbab1d95f8818de98df8989ceaec36596e820
Bell <bell@Bellui-MacBookPro.local>
2024-04-16 09:55:11 +0900

    [v0.0.1] 불필요한 코드 & library 제거

--- server.ts
+++ server.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import rtsp from "./lib/rtsp";
-import action from "./route/index";
+import route from "./route/index";
 import interceptor from "./interceptor/index";
 import errorHandle from "./interceptor/error";
 import helmet from "helmet";
@@ -10,14 +10,7 @@ import "./lib/schedule";
 
 const app = express();
 const http = require("http").Server(app);
-const io = require("socket.io")(http);
 const port = 4000;
-
-// 특정 도메인 설정
-// let corsOptions = {
-//   origin: 'https://www.domain.com',
-//   credentials: true
-// }
 
 app.use(cors());
 app.use(helmet());
@@ -25,19 +18,7 @@ app.use(express.json());
 app.use(interceptor);
 app.use(errorHandle);
 
-io.on("connection", (socket: any) => {
-  console.log("user connected", socket.id);
-
-  socket.on("disconnect", () => {
-    console.log("user disconnect", socket.id);
-  });
-
-  socket.on("data", (data: any) => {
-    console.log("data", data);
-  });
-});
-
-action(app);
+route(app);
 http.listen(port, () => {
   rtsp.init();
   console.log(`connected at ${port}`);
